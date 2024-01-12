9491524b0d9a3dbaf29b4c6f65a778817b60e157
mfathur <mfathur17@gmail.com>
2024-01-13 03:12:21 +0700

    fix: nav menu state

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,17 +3,13 @@
 import { SIDENAV_ITEMS } from "@/utils/constant";
 import MenuItem from "./MenuItem";
 
-type Props = {};
-
-const SideBar = (props: Props) => {
+const SideBar = () => {
   const className =
-    "md:w-60 h-screen flex-1 fixed hidden md:flex bg_primary pt-8";
-  const show = true;
-  const appendClass = show ? " ml-0" : " ml-[-250px] md:ml-0";
+    "md:w-60 h-screen flex-1 fixed hidden md:flex bg_primary pt-8 ml-0";
 
   return (
     <>
-      <nav className={`${className} ${appendClass}`}>
+      <nav className={`${className}`}>
         <div className="flex flex-col pr-11 lg:pr-0">
           {SIDENAV_ITEMS.map((sideNav, idx) => (
             <MenuItem key={`${sideNav.title}-${idx}`} {...sideNav} />
